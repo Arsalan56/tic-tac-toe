48b7e7a55c4a13a382eec235e27bf0c93d1d5cfa
Arsalan <118858748+Arsalan56@users.noreply.github.com>
2023-06-10 13:02:48 -0400

    Create player factory and game module
    A factory function is created, which stands as the base template for
    each player. A game module is created, which will store the necessary
    information related to the game's logic. So far, only the ability to
    change the array's values is possible.

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 const boxes = document.querySelectorAll('.box');
 // true = X, false = O
 let turn = true;
+// eslint-disable-next-line prefer-const
+
+// Player factory function
+const Player = (icon) => {
+    const score = document.querySelector(`.${icon}-score`);
+    const addWin = () => {
+        score.textContent++;
+    };
+    return { icon, addWin };
+};
+
+const Game = (() => {
+    const gameboard = [null, null, null, null, null, null, null, null, null];
+
+    const Add = (index, icon) => {
+        gameboard[index] = icon;
+    };
+
+    return { Add };
+})();
+
+const playerX = Player('x');
+const playerO = Player('o');
+
 boxes.forEach((box) =>
     box.addEventListener('click', () => {
         const activeImg = box.firstElementChild;
