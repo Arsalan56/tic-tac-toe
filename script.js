0aeee49b73f8dca9b18e8ea53e415136b2186076
Arsalan <118858748+Arsalan56@users.noreply.github.com>
2023-06-11 20:44:05 -0400

    Make scoreboard show whose turn it is
    The player's text on the scoreboard gets big when it's their turn, and
    returns to normal once it's the other player's turn. The box event
    listener is added inside the game module.

--- script.js
+++ script.js
@@ -1,41 +1,51 @@
-const boxes = document.querySelectorAll('.box');
 // true = X, false = O
 let turn = true;
 // eslint-disable-next-line prefer-const
 
 // Player factory function
 const Player = (icon) => {
+    const player = document.querySelector(`.scoreBoard > .player-${icon}`);
     const score = document.querySelector(`.${icon}-score`);
     const addWin = () => {
         score.textContent++;
     };
-    return { icon, addWin };
-};
-
-const Game = (() => {
-    const gameboard = [null, null, null, null, null, null, null, null, null];
-
-    const Add = (index, icon) => {
-        gameboard[index] = icon;
+    const toggle = () => {
+        player.classList.toggle('turn');
     };
-
-    return { Add };
-})();
+    return { icon, addWin, toggle };
+};
 
 const playerX = Player('x');
 const playerO = Player('o');
 
-boxes.forEach((box) =>
-    box.addEventListener('click', () => {
-        const activeImg = box.firstElementChild;
-        if (!activeImg.getAttribute('src')) {
-            if (turn) {
-                activeImg.setAttribute('src', 'svg-icons/x-icon.svg');
-                turn = false;
-            } else {
-                activeImg.setAttribute('src', 'svg-icons/o-icon.svg');
-                turn = true;
+const Game = ((p1, p2) => {
+    const gameActive = true;
+    const gameboard = [null, null, null, null, null, null, null, null, null];
+    const boxes = document.querySelectorAll('.box');
+    boxes.forEach((box) =>
+        box.addEventListener('click', () => {
+            const activeImg = box.firstElementChild;
+            if (!activeImg.getAttribute('src') && gameActive) {
+                if (turn) {
+                    activeImg.setAttribute('src', 'svg-icons/x-icon.svg');
+                    console.log(p1.classList);
+                    turn = false;
+                } else {
+                    activeImg.setAttribute('src', 'svg-icons/o-icon.svg');
+
+                    turn = true;
+                }
+                p1.toggle();
+                p2.toggle();
             }
-        }
-    })
-);
+        })
+    );
+    // const Add = (index, icon) => {
+    //     gameboard[index] = icon;
+    // };
+
+    const checkWin = () => {};
+    // return { Add };
+})(playerX, playerO);
+
+// Game;
